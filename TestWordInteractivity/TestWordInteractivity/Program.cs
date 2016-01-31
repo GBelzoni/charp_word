37dee66dff29d9d2e7a1ab29b7bf78515336c1b0
Patrick Henry Costello <patrickhcostello@gmail.com>
2016-01-31 21:52:42 +1100

    xmlwriterworking

---
+++ TestWordInteractivity/TestWordInteractivity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace TestWordInteractivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = ("    ");
+            settings.CloseOutput = true;
+            settings.OmitXmlDeclaration = true;
+            using (XmlWriter thisWriter = XmlWriter.Create(@"C:\Users\patri\Source\Repos\charp_word\TestWordInteractivity\dummyOptions.xml",settings))
+            {
+                
+
+                thisWriter.WriteStartElement("ListBox1");
+                for (int i = 0; i < 2000; i++)
+                {
+                    string thisVal = "Option" + i.ToString();
+
+                    thisWriter.WriteElementString("value", thisVal);
+
+                }
+                thisWriter.WriteEndElement();
+                thisWriter.Flush();
+            }
+        }
+    }
+}
